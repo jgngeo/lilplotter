61d2c5a0c4517c99af4470e12067b9e3c2ce2f3a
Jugunu George <find.jgn@gmail.com>
2016-01-31 14:11:20 +0530

    Added the MPU Sketch and Rearranged the config files

---
+++ arduino_sketches/mpu_6050_sketch/mpu_6050_sketch.ino
@@ -0,0 +1,68 @@
+// MPU-6050 Short Example Sketch
+// By Arduino User JohnChi
+// August 17, 2014
+// Public Domain
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+float tempdeg = 0.0;
+
+struct packet {
+  uint32_t sof;
+  uint32_t len;
+  uint32_t crc;
+  uint16_t val[6];
+  float tmpdeg;
+  uint32_t eof;
+}pkt;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(115200);
+  //Serial.print(sizeof(pkt));
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  tempdeg = Tmp/340.00+36.53;
+  
+  pkt.sof = 0xC0DEFACE;
+  pkt.len = sizeof(pkt) - 16;
+  pkt.crc = 0x00000000;
+  pkt.val[0] = AcX;
+  pkt.val[1] = AcY;
+  pkt.val[2] = AcZ;
+  pkt.val[3] = GyX;
+  pkt.val[4] = GyY;
+  pkt.val[5] = GyZ;
+  pkt.tmpdeg = tempdeg;
+  pkt.eof = 0xDA7C105E;
+  
+  #if 1
+  Serial.write((uint8_t *)&pkt, sizeof(pkt));
+  #else
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(tempdeg);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  
+  #endif
+  //delay(50);
+}
